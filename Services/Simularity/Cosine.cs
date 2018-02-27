f52126063b98b3f54f48f8271b8b5795ef03c986
Maarten de Graaf <wiejeben@gmail.com>
2018-02-27 11:09:12 +0100

    Initial commit

---
+++ Services/Simularity/Cosine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataScience.Services.Simularity
+{
+    public class Cosine : Simularity
+    {
+        public Cosine(List<Tuple<double, double>> values) : base(values)
+        {
+        }
+
+        public Cosine(IReadOnlyList<double> x, IReadOnlyList<double> y) : base(x, y)
+        {
+        }
+
+        public override double Calculate()
+        {
+            double multiSum = 0;
+            double sumX2 = 0;
+            double sumY2 = 0;
+
+            foreach (var value in Values)
+            {
+                multiSum += value.Item1 * value.Item2;
+
+                sumX2 += Math.Pow(value.Item1, 2);
+                sumY2 += Math.Pow(value.Item2, 2);
+            }
+
+            return multiSum / (Math.Sqrt(sumX2) * Math.Sqrt(sumY2));
+        }
+    }
+}
---
+++ Services/Simularity/Simularity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataScience.Services.Simularity
+{
+    public abstract class Simularity
+    {
+        protected readonly List<Tuple<double, double>> Values;
+
+        protected Simularity(List<Tuple<double, double>> values)
+        {
+            Values = values;
+        }
+
+        protected Simularity(IReadOnlyList<double> x, IReadOnlyList<double> y)
+        {
+            Values = SimularityList.Create(x, y);
+        }
+        
+        public abstract double Calculate();
+    }
+}
