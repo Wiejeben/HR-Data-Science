199d702f486a4cad54a0c0c583814c042ef25d4d
Maarten de Graaf <wiejeben@gmail.com>
2018-04-23 18:08:11 +0200

    Add scatter plot

--- Data-Mining/wwwroot/js/site.js
+++ Data-Mining/wwwroot/js/site.js
@@ -1,38 +1,48 @@
 ﻿var ctx = document.getElementById("myChart").getContext('2d');
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var scatterChart = new Chart(ctx, {
+    type: 'scatter',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Cluster 1',
+            backgroundColor: 'green',
+            borderWidth: 0,
+            pointRadius: 5,
+            data: [{
+                x: -10,
+                y: 0,
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }]
+        }, {
+            label: 'Cluster 2',
+            backgroundColor: 'red',
+            borderWidth: 0,
+            pointRadius: 5,
+            data: [{
+                x: 10,
+                y: 20
+            }, {
+                x: 15,
+                y: 10
+            }]
         }]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
             }]
+        },
+        legend: {
+            labels: {
+                fontSize: 20,
+            }
         }
     }
 });
