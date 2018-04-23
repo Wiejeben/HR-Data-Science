002833f8ac3f75260f2afed66e2f762f9fd729d4
Maarten de Graaf <wiejeben@gmail.com>
2018-04-23 18:13:14 +0200

    Use data from template

--- Data-Mining/wwwroot/js/site.js
+++ Data-Mining/wwwroot/js/site.js
@@ -3,34 +3,7 @@
 var scatterChart = new Chart(ctx, {
     type: 'scatter',
     data: {
-        datasets: [{
-            label: 'Cluster 1',
-            backgroundColor: 'green',
-            borderWidth: 0,
-            pointRadius: 5,
-            data: [{
-                x: -10,
-                y: 0,
-            }, {
-                x: 0,
-                y: 10
-            }, {
-                x: 10,
-                y: 5
-            }]
-        }, {
-            label: 'Cluster 2',
-            backgroundColor: 'red',
-            borderWidth: 0,
-            pointRadius: 5,
-            data: [{
-                x: 10,
-                y: 20
-            }, {
-                x: 15,
-                y: 10
-            }]
-        }]
+        datasets: window.datasets
     },
     options: {
         scales: {
