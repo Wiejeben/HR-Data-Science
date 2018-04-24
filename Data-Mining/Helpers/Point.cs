64db38d6c612259a194cc3bc2ab6113af8881ee5
Maarten de Graaf <wiejeben@gmail.com>
2018-04-24 11:47:49 +0200

    Implement dataset and visualize in scatter plot

---
+++ Data-Mining/Helpers/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace Data_Mining.Helpers
+{
+    public class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public override string ToString()
+        {
+            return "{ x:  " + X + ", y: " + Y + " }";
+        }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
--- Data-Mining/Pages/Cluster.cshtml.cs
+++ Data-Mining/Pages/Cluster.cshtml.cs
@@ -1,8 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Data_Mining.Helpers;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace Data_Mining.Pages
 {
     public class ClusterModel : PageModel
     {
+        public List<Point> Points { get; private set; }
+        
+        public void OnGet()
+        {
+            var data = new List<Point>();
+            // Read dataset
+            using (var reader = new StreamReader("datasets/WineData.csv"))
+            {
+                int height = 0;
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+
+                    int width = 0;
+                    foreach (string value in values)
+                    {
+                        if (value == "1")
+                        {
+                            // Register coordinates of locations where the value is "1"
+                            data.Add(new Point(width, height));
+                        }
+                        width++;
+                    }
+                    height++;
+                }
+            }
+
+            Points = data;
+        }
+
+        public string PointsToJson()
+        {
+            var result = "";
+            
+            foreach (var point in Points)
+            {
+                result += point + ",";
+            }
+            
+            return "[" + result + "]";
+        }
     }
 }
