87053806b66d9c87295197c35a48a33289364969
Maarten de Graaf <wiejeben@gmail.com>
2018-04-24 15:31:02 +0200

    Add dots that will be placed randomly onto our plot

--- Data-Mining/Pages/Cluster.cshtml.cs
+++ Data-Mining/Pages/Cluster.cshtml.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Data_Mining.Helpers;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.System.Collections.Sequences;
 
 namespace Data_Mining.Pages
 {
     public class ClusterModel : PageModel
     {
         public List<Point> Points { get; private set; }
+        public int MaxHeight { get; set; }
+        public int MaxWidth { get; set; }
 
         public void OnGet()
         {
@@ -33,19 +37,39 @@ namespace Data_Mining.Pages
                         width++;
                     }
 
+                    MaxWidth = width;
                     height++;
                 }
+                MaxHeight = height; 
             }
 
             Points = data;
         }
 
+        /**
+         * Output points as JSON array.
+         */
         public string PointsToJson()
         {
             var result = "";
 
             foreach (var point in Points)
             {
+                result += point.ToJson() + ",";
+            }
+
+            return "[" + result + "]";
+        }
+
+        public string RandomPositions(int amount)
+        {
+            var result = "";
+            var random = new Random();
+
+            for (int i = 0; i < amount; i++)
+            {
+                var point = new Point(random.Next(0, MaxWidth), random.Next(0, MaxHeight));
+
                 result += point.ToJson() + ",";
             }
 
